2e452583a031d51ed0387eab333a2616fadbd2e5
Sian Caleb Domasig <siancaleb@gmail.com>
2022-04-07 16:16:26 +0800

    Add MainUIHandler

---
+++ Breakout Game/Assets/Scripts/MainUIHandler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MainUIHandler : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI highScoreText;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
