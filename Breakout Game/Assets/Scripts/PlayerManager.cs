83fbbb4a27b370ad7927c0140a7103a1af21af6e
Sian Caleb Domasig <siancaleb@gmail.com>
2022-04-07 15:33:49 +0800

    Add load and save methods

--- Breakout Game/Assets/Scripts/PlayerManager.cs
+++ Breakout Game/Assets/Scripts/PlayerManager.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
     public PlayerManager Instance;
+
+    public string currentName;
+    public int currentScore;
+
+    public string bestName;
+    public int bestScore;
 
     void Start()
     {
@@ -15,10 +22,37 @@ public class PlayerManager : MonoBehaviour
         }
 
         Instance = this;
+
+        LoadBestData();
     }
 
-    void Update()
+    [System.Serializable]
+    class SaveData
     {
-        
+        public string bestName;
+        public int bestScore;
+    }
+
+    public void SaveBestData()
+    {
+        SaveData data = new SaveData();
+        data.bestName = bestName;
+        data.bestScore = bestScore;
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadBestData()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            bestName = data.bestName;
+            bestScore = data.bestScore;
+        }
     }
 }
